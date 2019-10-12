734c9a4cfdd0194c327b6e0015e19e040ff3392b
shamiul94 <1505038.sh@ugrad.cse.buet.ac.bd>
2019-10-12 15:48:12 +0600

    #v2.0 arguments augmented.

--- README.md
+++ README.md
@@ -1,1 +1,52 @@
-Subtitle Synchronizer
+# Subtitle Synchronizer
+This is a Python3 script written by me out of laziness to synchronize subtitles for movies or TV series or anime or others. In VLC and other media players there are UI based options to sync them with the video but they seems time consuming to use to me, doesn't have clear instructions and doesn't work properly. I could never make use of it. This file takes care of this problem. 
+
+# How to run this file
+## What do you need to run this file
+1. Python3 
+2. Subtitle files
+
+## How to run it
+This code takes two parameters. 
+You can run this line from command line to know how to use it. 
+
+```python
+python3 subsync.py -h
+```
+
+Output says - 
+
+```python
+usage: subsync.py [-h] -f FILENAME -t DIFF
+
+optional arguments:
+  -h, --help            			show this help message and exit
+  -f FILENAME, --file FILENAME 		input subtitle file name
+  -t DIFF, --time DIFF  input 		positive (speed up subtitle) or negative number (speed down subtitle)
+```
+
+## Example usage
+At first, copy this python file in the directory the subtitle file is in. 
+Or, if you are in Linux and if you feel like it, keep this python file in a directory and add that path to the `~/.bashrc` file. Then go to the directory your subtitle file is in. 
+
+### Go FORWARD
+Suppose you are in a directory which has a subtitle file named `subtitle.srt`. Now you want to fasten (go forward) this subtitle by 15 seconds. You have to run this line - 
+
+```python
+python3 subsync.py -f subtitle.srt -t 15
+```
+Your subtitle file will be modified and changed.
+
+### Go BACKWORD
+Suppose you want to backword by 15 seconds. Just input a negative number as the time value (-15). For example - 
+
+```python
+python3 subsync.py -f subtitle.srt -t -15
+```
+ 
+
+## Note
+1. You can also use `--file` instead of `-f` and `--time` instead of `-t`. 
+2. If you find any bug feel free to create an issue or mail me at `shamiulhasan93@gmail.com`. 
+
+Thank you. 
---
+++ subsync.py
@@ -0,0 +1,75 @@
+# @author: Shamiul Hasan (Heisenberg), BUET CSE
+# Date: 3:45 PM (BDT), Oct 12, 2019
+# Email: shamiulhasan93@gmail.com
+# github: shamiul94
+
+
+import os
+from argparse import ArgumentParser
+
+parser = ArgumentParser()
+
+parser.add_argument("-f", "--file", type=str, dest="filename", required=True,
+                    help="input subtitle file name")
+
+parser.add_argument("-t", "--time",
+                    required=True, type=int, dest="diff", default=0,
+                    help="input positive (speed up subtitle) or negative number (speed down subtitle)")
+
+args = parser.parse_args()
+dir_path = str(os.path.dirname(os.path.realpath(__file__)))
+diff = args.diff
+filename = dir_path + '/' + args.filename
+
+with open(filename) as f:
+    content = f.readlines()
+
+content = [x for x in content]
+
+fw = open(filename, "w")
+for x in content:
+    if x.strip().__contains__('-->'):
+        line = x.strip().split(' --> ')
+        first = line[0]
+        second = line[1]
+
+        firstTimeWithoutMS = first.split(',')[0]
+        firstMS = first.split(',')[1]
+        secondTimeWithoutMS = second.split(',')[0]
+        secondMS = second.split(',')[1]
+
+        firstHour = int(firstTimeWithoutMS.split(':')[0])
+        firstMinute = int(firstTimeWithoutMS.split(':')[1])
+        firstSecond = int(firstTimeWithoutMS.split(':')[2])
+
+        secondHour = int(secondTimeWithoutMS.split(':')[0])
+        secondMinute = int(secondTimeWithoutMS.split(':')[1])
+        secondSecond = int(secondTimeWithoutMS.split(':')[2])
+
+        firstTime = firstHour * 60 * 60 + firstMinute * 60 + firstSecond + diff
+        secondTime = secondHour * 60 * 60 + secondMinute * 60 + secondSecond + diff
+
+        firstHour = int(firstTime / 3600)
+        firstMinute = int((firstTime % 3600) / 60)
+        firstSecond = int(((firstTime % 3600) % 60))
+
+        secondHour = int(secondTime / 3600)
+        secondMinute = int((secondTime % 3600) / 60)
+        secondSecond = int(((secondTime % 3600) % 60))
+
+        # format = 02:39:53,500 --> 02:39:54,859
+
+        finalLine = str(firstHour).zfill(2) + ':' \
+                    + str(firstMinute).zfill(2) + ':' \
+                    + str(firstSecond).zfill(2) + ',' \
+                    + firstMS + ' --> ' \
+                    + str(secondHour).zfill(2) + ':' \
+                    + str(secondMinute).zfill(2) + ':' \
+                    + str(secondSecond).zfill(2) + ',' + secondMS
+
+        fw.write(finalLine + '\n')
+
+    else:
+        fw.write(x)
+
+fw.close()
