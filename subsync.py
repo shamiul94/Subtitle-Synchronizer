9efbd5a053992df9721ac389d5e290e5360bc60f
shamiul94 <1505038.sh@ugrad.cse.buet.ac.bd>
2019-10-15 01:27:33 +0600

    #v2.1 backwarding discrepancy

--- subsync.py
+++ subsync.py
@@ -5,6 +5,7 @@
 
 
 import os
+import sys
 from argparse import ArgumentParser
 
 parser = ArgumentParser()
@@ -14,7 +15,7 @@ parser.add_argument("-f", "--file", type=str, dest="filename", required=True,
 
 parser.add_argument("-t", "--time",
                     required=True, type=int, dest="diff", default=0,
-                    help="input positive (speed up subtitle) or negative (speed down subtitle) number of seconds.")
+                    help="input positive (speed up subtitle) or negative (speed down subtitle) number of seconds")
 
 args = parser.parse_args()
 dir_path = str(os.path.dirname(os.path.realpath(__file__)))
@@ -49,6 +50,22 @@ for x in content:
         firstTime = firstHour * 60 * 60 + firstMinute * 60 + firstSecond + diff
         secondTime = secondHour * 60 * 60 + secondMinute * 60 + secondSecond + diff
 
+        if firstTime < 0 or secondTime < 0:
+            print('\n')
+            print('#' * 50)
+            print('Synchronization Not Possible. Your desired output goes under 00:00:00. File Unchanged.')
+            print('#' * 50)
+            print('First 10 lines of the original file is:\n')
+            print('*'*50)
+            fw.close()
+            fw = open(filename, "w")
+            for i in range(10):
+                print(content[i])
+            for y in content:
+                fw.write(y)
+            fw.close()
+            sys.exit()
+
         firstHour = int(firstTime / 3600)
         firstMinute = int((firstTime % 3600) / 60)
         firstSecond = int(((firstTime % 3600) % 60))
@@ -73,3 +90,18 @@ for x in content:
         fw.write(x)
 
 fw.close()
+
+print('\n')
+print('#' * 50)
+print('Subtitle File Successfully Synchronized!!!')
+print('#' * 50)
+
+print('\nFirst 10 lines of the generated file is: \n')
+print('*' * 50)
+
+N = 10
+with open(filename) as f:
+    content = f.readlines()
+
+for i in range(N):
+    print(content[i])
